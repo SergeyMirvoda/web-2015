d8ea5c6a27108167d311acf532b88670ea32a006
Sergey Mirvoda <mirvoda_sg@octonica.corp>
2015-04-22 22:28:27 +0500

    Лекция JS + canvas. The beginning. Rectangles

---
+++ lectures/javascript-canvas/src/script.js
@@ -0,0 +1,11 @@
+function initiate() {
+    var element = document.getElementById("canvas");
+    var canvas = element.getContext('2d');
+    canvas.fillStyle = "#0000ff";
+    canvas.strokeStyle = "#ff0000";
+    canvas.strokeRect(100, 100, 120, 120);
+    canvas.fillRect(110, 110, 100, 100);
+    canvas.clearRect(130, 130, 30, 30);
+}
+
+addEventListener("load", initiate);
