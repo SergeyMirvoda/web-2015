3a253a87eb9236fba97a6c8730652ec4a85de2f2
Sergey Mirvoda <sergey@mirvoda.com>
2015-05-19 01:40:04 +0500

    Исходники для задания 4

---
+++ task4/src/RoachFrontend/Scripts/game.js
@@ -0,0 +1,8 @@
+﻿function initiate() {
+    var element = document.getElementById("canvas");
+    var canvas = element.getContext('2d');
+    canvas.font = "bold 20px segoe UI";
+    canvas.fillText("Здесь должна быть Ваша игра", 100, 250);
+}
+
+addEventListener("load", initiate);
